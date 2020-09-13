8a13a05e972da37062c1e0d80895a80e5040b8aa
Paulo Ogliano <contato@pauloogliano.com>
2020-09-13 12:09:01 +0100

    challenge solved

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const subtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return acc + subtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQtd = products.reduce((acc, product) => {
+      const qtd = product.quantity;
 
-    return 0;
+      return acc + qtd;
+    }, 0);
+
+    return totalQtd;
   }, [products]);
 
   return (
